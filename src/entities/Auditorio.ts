eb864a8e5d27e3e9fd8ce1e005456f4843c3f3e5
Alexandro Oliveira <=>
2025-01-30 15:57:24 -0300

    Criado o entities para auditório.

--- src/entities/Auditorio.ts
+++ src/entities/Auditorio.ts
@@ -1,7 +1,30 @@
-class Auditorio {
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
-    // Alexandro Oliveira 
-    
+@Entity("auditoriums") 
+
+export class Auditorium {
+
+  @PrimaryGeneratedColumn() 
+  id: number;
+
+  @Column({ type: "varchar", nullable: false }) 
+  name: string;
+
+  @Column({ type: "int", nullable: false }) 
+  capacity: number;
+
+  @Column({ type: "varchar", nullable: false })
+  location: string;
+
+  @Column({ type: "boolean", default: false }) 
+  has_projector: boolean;
+
+  @Column({ type: "boolean", default: false }) 
+  has_sound_system: boolean;
+
+  @CreateDateColumn() 
+  created_at: Date;
+
+  @UpdateDateColumn() 
+  updated_at: Date;
 }
-
-export default Auditorio;
