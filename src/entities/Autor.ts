aeea700d99711a684b8d13d75c661f639911e645
Patella <jfpatella@gmail.com>
2025-03-07 16:18:35 -0300

    feat: implementa entidade authors.

--- src/entities/Autor.ts
+++ src/entities/Autor.ts
@@ -1,5 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("authors")
 class Autor {
-    
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "varchar",
+    length: 200,
+    nullable: false,
+  })
+  name: string;
+
+  @Column({ type: "timestamp" })
+  birthdate: Date;
+
+  @Column({ type: "varchar" })
+  biography: string;
+
+  @Column({
+    type: "varchar",
+    length: 50,
+    nullable: false,
+  })
+  nationality: string;
+
+  @Column({ type: "boolean" })
+  active: boolean;
+
+  @CreateDateColumn({ type: "timestamp", default: "now()" })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: "timestamp", onUpdate: "now()", nullable: true })
+  updated_at: Date;
 }
 
 export default Autor;
